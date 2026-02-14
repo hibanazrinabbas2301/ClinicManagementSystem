ec69898dee8bb2b5650c806ffad6ced39501dfb4
SIVIN WILLS <sivinwillssivin@gmail.com>
2026-02-14 11:53:39 +0530

    Added pharmacist controller

---
+++ ClinicManagementSystemMVC/ClinicManagementSystem/Controllers/Pharmacist.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class Pharmacist : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
